3163441dd3e2ed60ba6e07fc1b88ff63a657be80
jessieharrell <58713138+jessieharrell@users.noreply.github.com>
2019-12-12 13:41:20 -0800

    scnes

---
+++ src/edu/miracosta/cs112/swamis/view/AddCruiserScene.java
@@ -0,0 +1,127 @@
+package edu.miracosta.cs112.swamis;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class AddCruiserScene extends Scene {
+
+    private Scene mPrevScene;
+
+    private Controller controller = Controller.getInstance();
+    private ObservableList<SkateBoards> allSkateBoardsList;
+
+    private TextField trucksTF = new TextField();
+    private Label trucksLabel = new Label("Truck Brand: ");
+
+    private TextField wheelsTF = new TextField();
+    private Label wheelsLabel = new Label("Wheel Type: ");
+
+    private TextField priceTF = new TextField();
+    private Label priceLabel = new Label("Price of Cruiser: ");
+
+    private TextField materialTF = new TextField();
+    private Label materialLabel = new Label("*Must enter material*");
+
+    private TextField deckSizeTF = new TextField();
+    private Label deckSizeLabel = new Label("*Must enter deck size*");
+
+    private TextField truckSizeTF = new TextField();
+    private Label truckSizeLabel = new Label("*Must enter truck size*");
+
+
+    private Button saveButton = new Button("Save");
+    private Button cancelButton = new Button("Cancel");
+
+
+
+    public AddCruiserScene(Scene prevScene)
+    {
+        super(new GridPane(), 400, 300);
+        mPrevScene = prevScene;
+
+        GridPane pane = new GridPane();
+        pane.setHgap(15.0);
+        pane.setVgap(15.0);
+        pane.setPadding(new Insets(5));
+
+        pane.add(new Label("Trucks:"), 0, 0);
+        pane.add(trucksTF, 1, 0);
+        pane.add(trucksLabel, 2, 0);
+        trucksLabel.setVisible(false);
+
+        pane.add(new Label("Wheels:"), 0, 1);
+        pane.add(wheelsTF, 1, 1);
+        pane.add(wheelsLabel, 2, 1);
+        wheelsLabel.setVisible(false);
+
+        pane.add(new Label("Price:"), 0, 2);
+        pane.add(priceTF, 1, 2);
+
+        pane.add(new Label("Wood or Metal:"), 0, 3);
+        pane.add(materialTF, 1, 3);
+
+        pane.add(new Label("Deck Size (inches):"), 0, 4);
+        pane.add(deckSizeTF, 1, 4);
+
+        pane.add(new Label("Truck Size:"), 0, 5);
+        pane.add(truckSizeTF, 1, 5);
+
+        pane.add(cancelButton, 0, 8);
+        pane.add(saveButton, 1, 8);
+
+        saveButton.setOnAction(event -> save());
+        cancelButton.setOnAction(event -> goBackToPrevScene());
+        this.setRoot(pane);
+    }
+
+
+    private void save()
+    {
+        String trucks, wheels, material;
+        double price = 0.0;
+        int deckSize = 0, truckSize = 0;
+
+        trucks = trucksTF.getText();
+        trucksLabel.setVisible(trucks.isEmpty());
+
+        wheels = wheelsTF.getText();
+        wheelsLabel.setVisible(wheels.isEmpty());
+
+        material = materialTF.getText();
+
+
+        try {
+            price = Double.parseDouble(priceTF.getText());
+            if (price < 0.0)
+                priceLabel.setVisible(true);
+        }
+        catch (NumberFormatException e) {  priceLabel.setVisible(true); }
+
+        try {
+            truckSize = Integer.parseInt(truckSizeTF.getText());
+        }
+        catch (NumberFormatException e) { }
+
+        try {
+            deckSize = Integer.parseInt(deckSizeTF.getText());
+        }
+        catch (NumberFormatException e) { }
+
+
+        Trick t = new Trick(trucks, wheels, price, deckSize, material, truckSize);
+        controller.getAllBoards().add(t);
+        goBackToPrevScene();
+    }
+
+    private void goBackToPrevScene()
+    {
+        ViewNavigator.loadScene("Swamis Shopping Cart", mPrevScene);
+    }
+
+
+}
--- src/edu/miracosta/cs112/swamis/view/MainScene.java
+++ src/edu/miracosta/cs112/swamis/view/MainScene.java
@@ -1,47 +1,39 @@
-package edu.miracosta.cs112.swamis.view;
+package edu.miracosta.cs112.swamis;
 
-import edu.miracosta.cs112.swamis.controller.Controller;
-import edu.miracosta.cs112.swamis.model.OceanBoards;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
+import java.awt.*;
+
 public class MainScene extends Scene {
-// TODO J UNIT TESTS!
-    public static final int HEIGHT = 500;
-    public static final int WIDTH = 750;
 
-    private ImageView swamisIV = new ImageView(); // to use images in application
+    public static final int WIDTH = 700;
+    public static final int HEIGHT = 700;
 
-    private Button addSurfBoardButton = new Button("+ Add SurfBoard");
-    private Button addBodyBoardButton = new Button("+ Add BodyBoard");
+    private javafx.scene.control.Button addCruiserButton = new javafx.scene.control.Button("+ Add Cruiser");
+    private javafx.scene.control.Button addTrickButton = new javafx.scene.control.Button("+ Add Trick Skateboard");
 
-    private ListView<OceanBoards> boardsLV = new ListView<>();
-
-    private Button removeButton = new Button("Remove From Shopping Cart");
+    private ListView<SkateBoards> sk8BoardsLV = new ListView<>();
+    private javafx.scene.control.Button removeButton = new javafx.scene.control.Button("Remove From Shopping Cart");
 
     private Controller controller = Controller.getInstance();
-    private ObservableList<OceanBoards> oceanBoardList;
-    private OceanBoards selectedItem;
+    private ObservableList<SkateBoards> skateBoardsList;
+    private SkateBoards selectedItem;
 
-    public MainScene()
-    {
+    public MainScene() {
         super(new GridPane(), WIDTH, HEIGHT);
 
-        oceanBoardList = controller.getAllBoards();
-        boardsLV.setItems(oceanBoardList);
-        boardsLV.setPrefWidth(750);
+        skateBoardsList = controller.getAllBoards();
+        sk8BoardsLV.setItems(skateBoardsList);
 
-        boardsLV.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<OceanBoards>() {
+        sk8BoardsLV.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<SkateBoards>() {
             @Override
-            public void changed(ObservableValue<? extends OceanBoards> observable, OceanBoards oldValue, OceanBoards newValue) {
+            public void changed(ObservableValue<? extends SkateBoards> observable, SkateBoards oldValue, SkateBoards newValue) {
                 removeButton.setDisable(true);
                 if (newValue != null) {
                     selectedItem = newValue;
@@ -50,62 +42,51 @@ public class MainScene extends Scene {
             }
         });
 
-        addBodyBoardButton.setOnAction(event -> addBodyBoard());
-        addSurfBoardButton.setOnAction(event -> addSurfBoard());
+        addCruiserButton.setOnAction(event -> addCruiser());
+        addTrickButton.setOnAction(event -> addTrick());
         removeButton.setOnAction(event -> removeItem());
 
         GridPane pane = new GridPane();
-
-        swamisIV.setImage(new Image("swamis.png"));
-        swamisIV.setFitWidth(WIDTH); // width constant for size of window
-        swamisIV.setFitHeight(250);
-        pane.setHgap(10.0);
-        pane.setVgap(5);
-        pane.setPadding(new Insets(5));
-        pane.add(swamisIV, 0, 0, 3, 1);
-
-        boardsLV.setPrefWidth(WIDTH);
-        boardsLV.setPrefHeight(HEIGHT/5.0);
-        pane.add(boardsLV, 0, 7, 3, 1); // TODO Format columns and rows accordingly for this!!
-
-        pane.add(addBodyBoardButton, 0, 6);
-        pane.add(addSurfBoardButton, 1, 6);
-        pane.add(removeButton, 0, 8);
+        sk8BoardsLV.setPrefWidth(WIDTH);
+        pane.add(sk8BoardsLV,0,8,3,1);
+        pane.add(addCruiserButton, 0, 5);
+        pane.add(addTrickButton, 1, 5);
+        pane.add(removeButton, 0, 20);
 
         setRoot(pane);
+
     }
 
     private void removeItem() {
-        //TODO: If the selected item is null, return
-        //TODO: Otherwise, remove the selected item from the list
-        //TODO: Update the display when done
 
         if (selectedItem == null)
             return;
 
-        oceanBoardList.remove(selectedItem);
+        skateBoardsList.remove(selectedItem);
         updateDisplay();
     }
 
-    private void addSurfBoard()
+    private void addCruiser()
     {
-        // Use the ViewNavigator to load the AddScene
-        ViewNavigator.loadScene("Add SurfBoard", new AddSurfBoardScene(this)); // use this because we are in MainScene currently
-        // Update the display when done.
+        ViewNavigator.loadScene("Add Cruiser Board", new AddCruiserScene(this));
         updateDisplay();
     }
 
-    private void addBodyBoard()
+    private void addTrick()
     {
-        // Use the ViewNavigator to load the AddScene
-        ViewNavigator.loadScene("Add BodyBoard", new AddBodyBoardScene(this)); // use this because we are in MainScene currently
-        // Update the display when done.
+        ViewNavigator.loadScene("Add Trick Board", new AddTrickScene(this));
         updateDisplay();
     }
 
-    private void updateDisplay()
-    {
-        boardsLV.refresh();
+    private void updateDisplay() {
+        sk8BoardsLV.refresh();
     }
-
 }
+
+
+
+
+
+
+
+
